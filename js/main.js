45988ee9876eab6ff9c162788ad272b349cba925
Zhao Jingchen <dracupid@gmail.com>
2015-01-03 21:54:48 +0800

    coomment

--- js/main.js
+++ js/main.js
@@ -19,6 +19,23 @@ function init() {
 	renderFooter();
 	renderHeader();
     playbgm();
+    var $ds = $('.ds-thread');
+    if($ds){
+    	$ds.data('url', location.href);
+    	initDuoshuo();
+    }
+}
+
+function initDuoshuo(){
+	duoshuoQuery = {short_name:"fei1005"};
+    (function() {
+        var ds = document.createElement('script');
+        ds.type = 'text/javascript';ds.async = true;
+        ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
+        ds.charset = 'UTF-8';
+        (document.getElementsByTagName('head')[0] 
+         || document.getElementsByTagName('body')[0]).appendChild(ds);
+    })();
 }
 
 function renderHeader() {
